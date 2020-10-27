280df95f0349f0d57b9299a29a0da583bf1f122e
Alexander Chernikov <Chernikov.A4@wildberries.ru>
2020-10-27 19:03:33 +0300

    init

---
+++ golang/0238_product_of_array_except_self.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))
+}
+
+/*
+i: 0
+left: 1
+right: 4
+res: [1 1 1 1]
+i: 1
+left: 2
+right: 12
+res: [1 1 4 1]
+i: 2
+left: 6
+right: 24
+res: [1 12 8 1]
+i: 3
+left: 24
+right: 24
+res: [24 12 8 6]
+[24 12 8 6]
+*/
+// Time complexity : O(N)
+// Space complexity : O(N)
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	for i := 0; i < len(res); i++ {
+		res[i] = 1
+	}
+	left := 1
+	right := 1
+	for i := 0; i < len(nums); i++ {
+		j := len(nums) - 1 - i
+		res[j] = res[j] * right
+		right = right * nums[j]
+		res[i] = res[i] * left
+		left = left * nums[i]
+		fmt.Printf("i: %d \n", i)
+		fmt.Printf("left: %d \n", left)
+		fmt.Printf("right: %d \n", right)
+		fmt.Printf("res: %d \n", res)
+	}
+
+	return res
+}
